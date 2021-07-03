e4fe259bb32ab2e541bb75e66dc8f229eea91e0b
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-03 19:46:39 -0400

    Added puppet installation

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -5,3 +5,6 @@ sudo yum -y install httpd
 
 # Start & Enable apache
 sudo systemctl start httpd && sudo systemctl enable httpd
+
+# Install Puppet
+sudo rpm -Uvh https://yum.puppet.com/puppet7-release-el-8.noarch.rpm
