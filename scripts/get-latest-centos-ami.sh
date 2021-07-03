7f1dd25b903beac44c7dc4e600ed9d409fb820fb
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-03 10:31:47 -0400

    wip

--- scripts/get-latest-centos-ami.sh
+++ scripts/get-latest-centos-ami.sh
@@ -2,5 +2,7 @@
 
 aws ec2 describe-images \
     --owners '125523088429' \
-    --filters 'Name=name,Values=CentOS 7*' 'Name=architecture,Values=x86_64' \
-    --query 'sort_by(Images, &CreationDate)[-1].[ImageId]' \
+    --filters 'Name=name,Values=CentOS 7.9*' 'Name=architecture,Values=x86_64' \
+    --query 'Images[*].[ImageId]' \
+    --output text
+    #--query 'sort_by(Images, &CreationDate)[-1].[ImageId]' \
