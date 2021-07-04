d4c31fe237b9515f381b42ef310911572fd39be4
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-04 09:54:49 -0400

    update  script

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -8,3 +8,11 @@ sudo systemctl start httpd && sudo systemctl enable httpd
 
 # Install Puppet
 sudo rpm -Uvh https://yum.puppet.com/puppet7-release-el-8.noarch.rpm
+
+# Install Puppet Agent
+sudo yum install puppet-agent -y
+
+# Add secure_path
+echo 'Defaults secure_path = /sbin:/bin:/usr/sbin/usr/bin:/opt/puppetlabs/bin' | sudo tee -a /etc/sudoers.d/extra
+
+bash
