1e2632239bb7234bf8b4937af0f958a5c11c8fdf
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-05 17:27:34 -0400

    wip

--- scripts/packer/ebs.sh
+++ scripts/packer/ebs.sh
@@ -7,14 +7,22 @@ block_device=`lsblk -a -d -e 230 | grep $? | cut -d' ' -f1 | sed -n 2p` && echo 
 vol_group_device=/dev/$block_device && echo $vol_group_device
 
 # Create a new partition
-#/bin/echo -e "d\n\nn\n\n\n\n\nw" | sudo fdisk $vol_group_device || true
-/bin/echo -e "n\nn\n\n\n\n\nw" | sudo fdisk $vol_group_device || true
+/bin/echo -e "d\n\nn\n\n\n\n\nw" | sudo fdisk $vol_group_device || true
 
 # Sync the disk
 sudo partprobe
 
+# Format the partition
+sudo mkfs -t ext4 $vol_group_device
+
 # Create new directory
 sudo mkdir /var/app
+
+# Mount app directory on new partition
+sudo mount -t auto $vol_group_device /var/app
+
+# Find parition UUID
+blkid
 
 # Create & Move index.html
 echo '<h1><center>Infrastructure as Code FTW</center></h1>' > /tmp/index.html
