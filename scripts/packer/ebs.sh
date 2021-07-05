7f10151dac1ee0b472bd86f1baa07ec71b014a1f
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-05 15:37:49 -0400

    wip

---
+++ scripts/packer/ebs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+# Find the block device
+block_device=`lsblk -a -d -e 230 | grep $? | cut -d' ' -f1 | sed -n 2p` && echo $block_device
+
+# Set the block device
+vol_group_device=/dev/$block_device && echo $vol_group_device
+
+# Create a new partition
+/bin/echo -e "d\n\nn\n\n\n\n\nw" | sudo fdisk $vol_group_device || true
+
+# Sync the disk
+sudo partprobe
+
+# add space to pv (which also adds free space to the VG)
+phy_vol=`sudo pvs | grep /dev | sed -r 's/^[ \t]*//' | cut -d' ' -f1` && echo $phy_vol
+sudo pvresize $phy_vol
