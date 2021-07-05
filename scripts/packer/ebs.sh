402e1e961e36bac7a01da7492313d77590ec7626
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-05 16:13:13 -0400

    wip

--- scripts/packer/ebs.sh
+++ scripts/packer/ebs.sh
@@ -7,11 +7,19 @@ block_device=`lsblk -a -d -e 230 | grep $? | cut -d' ' -f1 | sed -n 2p` && echo 
 vol_group_device=/dev/$block_device && echo $vol_group_device
 
 # Create a new partition
-/bin/echo -e "d\n\nn\n\n\n\n\nw" | sudo fdisk $vol_group_device || true
+#/bin/echo -e "d\n\nn\n\n\n\n\nw" | sudo fdisk $vol_group_device || true
+/bin/echo -e "n\nn\n\n\n\n\nw" | sudo fdisk $vol_group_device || true
 
 # Sync the disk
 sudo partprobe
 
 # add space to pv (which also adds free space to the VG)
-phy_vol=`sudo pvs | grep /dev | sed -r 's/^[ \t]*//' | cut -d' ' -f1` && echo $phy_vol
-sudo pvresize $phy_vol
+#phy_vol=`sudo pvs | grep /dev | sed -r 's/^[ \t]*//' | cut -d' ' -f1` && echo $phy_vol
+#sudo pvresize $phy_vol
+
+# Create new directory
+sudo mkdir /var/app
+
+# Create & Move index.html
+echo '<h1><center>Infrastructure as Code FTW</center></h1>' > /tmp/index.html
+sudo mv /tmp/index.html /var/www/html/
