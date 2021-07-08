f493ec6c65819def668d7ef7b80c239004cd3dc1
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-08 15:15:19 -0400

    added iam role

--- hashicorp/main.tf
+++ hashicorp/main.tf
@@ -81,3 +81,27 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_ssh"
   }
 }
+
+resource "aws_iam_role" "web_server" {
+  name = "web_server"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "web_server"
+  }
+}
