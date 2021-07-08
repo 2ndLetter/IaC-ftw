4e52f12c3ea5196dae34d77603833b9c23bf7460
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-08 10:52:48 -0400

    default public ip addresses on launch

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
@@ -13,8 +13,9 @@ resource "aws_vpc" "this" {
 }
 
 resource "aws_subnet" "this" {
-  vpc_id     = aws_vpc.this.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "IaC-FTW"
