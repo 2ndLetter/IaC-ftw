1969ae7cf8b17e32192a5c8afb804a254ea1896c
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-08 10:06:01 -0400

    wip

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,12 @@ output "PublicIP" {
   value       = aws_instance.my-instance.public_ip
 }
 
+output "SSH_SG_ID" {
+  description = "SSH Security Group ID"
+  value       = aws_security_group.allow_ssh.id
+}
+
+output "HTTP_SG_ID" {
+  description = "HTTP Security Group ID"
+  value       = aws_security_group.allow_http.id
+}
