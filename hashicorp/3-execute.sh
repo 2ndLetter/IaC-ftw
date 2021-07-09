0589c7a01e6e17024956d87d387b3ef1291384e5
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-09 06:13:34 -0400

    wip

--- hashicorp/3-execute.sh
+++ hashicorp/3-execute.sh
@@ -19,16 +19,16 @@ sleep 5
 terraform apply --auto-approve
 sleep 5
 
-## # Export required terraform outputs
-## export aws_subnet_id=$(terraform output -raw SUB_ID)
-## export aws_security_group=$(terraform output -raw SSH_SG_ID)
-## 
-## # Validate Packer template
-## packer validate packer-webserver-1.json
-## sleep 5
-## 
-## # Build AMI
-## packer build \
-## -var "aws_subnet_id=$aws_subnet_id" \
-## -var "aws_security_group=$aws_security_group" \
-## packer-webserver-1.json
+# Export required terraform outputs
+export aws_subnet_id=$(terraform output -raw SUB_ID)
+export aws_security_group=$(terraform output -raw SSH_SG_ID)
+
+# Validate Packer template
+packer validate packer-webserver-1.json
+sleep 5
+
+# Build AMI
+packer build \
+-var "aws_subnet_id=$aws_subnet_id" \
+-var "aws_security_group=$aws_security_group" \
+packer-webserver-1.json
