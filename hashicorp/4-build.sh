f8757491e185a2f32cce463491a804170367eee1
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-09 12:12:13 -0400

    wip

--- hashicorp/4-build.sh
+++ hashicorp/4-build.sh
@@ -5,7 +5,7 @@ if [[ $1 == new_bucket ]]
 then
   export BUCKET_NAME=iacftw-$(openssl rand -base64 48 | sed -e 's/+//g' -e 's/\///g' -e 's/[A-Z]//g')
   printf "bucket_name = \"$BUCKET_NAME\"" > terraform.tfvars
-  printf "bucket_name_build = \"$BUCKET_NAME\"" >> application/terraform.tfvars
+  printf "bucket_name = \"$BUCKET_NAME\"" >> application/terraform.tfvars
 else
   echo "No s3 bucket created/re-created, run \"./3-build.sh new_bucket\" if you need to create/re-create the application s3 bucket."
 fi
