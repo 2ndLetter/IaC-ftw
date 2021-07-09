9a6b11483a20711e021565f6e5aae889358c9547
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-09 16:13:10 -0400

    added chmod to keypair

--- hashicorp/3-keypair.sh
+++ hashicorp/3-keypair.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
 # Create keypair in aws account and local ~/.ssh folder
-aws ec2 create-key-pair --key-name $1 | jq '.[]' | sed -n '2 p' | sed -e 's/"//g' > ~/.ssh/$1.pem
+#aws ec2 create-key-pair --key-name $1 | jq '.[]' | sed -n '2 p' | sed -e 's/"//g' > ~/.ssh/$1.pem
+aws ec2 create-key-pair --key-name $1 --query "KeyMaterial" --output text > ~/.ssh/$1.pem
+
+chmod 400 ~/.ssh/$1.pem
 
 # Add keypair name to application/terraform.tfvars file
 echo "key_name = \"$1\"" > application/terraform.tfvars
