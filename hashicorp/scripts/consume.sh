f8cef04efdd4f7dcf95ee2f342851846c2200b1d
Brian Chadwick <b.matthew.chadwick@gmail.com>
2021-07-13 21:46:01 -0400

    fixed typo

--- hashicorp/scripts/consume.sh
+++ hashicorp/scripts/consume.sh
@@ -4,6 +4,6 @@ export APP_BUCKET=$(aws s3api list-buckets --query 'Buckets[?contains(Name, `iac
 
 export READ_WEBSERVER_IP=$(aws s3 cp s3://$APP_BUCKET/object.txt -)
 
-wget http://READ_WEBSERVER_IP -P /tmp
+wget http://$READ_WEBSERVER_IP -P /tmp
 
 sudo mv /tmp/index.html /var/www/html/index.html
